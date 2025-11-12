bad171057fd66495193725be38d1d4d1404211c6
cxfksword <718792+cxfksword@users.noreply.github.com>
2025-11-12 13:56:49 +0800

    ci: fix build error

--- Sources/BuildScripts/XCFrameworkBuild/base.swift
+++ Sources/BuildScripts/XCFrameworkBuild/base.swift
@@ -229,6 +229,7 @@ class BaseBuild {
                 "-DCMAKE_SYSTEM_PROCESSOR=\(arch.rawValue)",
                 "-DCMAKE_INSTALL_PREFIX=\(thinDirPath)",
                 "-DBUILD_SHARED_LIBS=0",
+                "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
             ]
             arguments.append(contentsOf: self.arguments(platform: platform, arch: arch))
             try Utility.launch(path: cmake, arguments: arguments, currentDirectoryURL: buildURL, environment: environ)
