16009080153c163a3bad775b9642238225162372
cxfksword <718792+cxfksword@users.noreply.github.com>
2026-01-13 21:30:02 +0800

    chore: raise minimum target to macOS 11.0 / iOS 14.0 / tvOS 14.0

--- docs/Package.template.swift
+++ docs/Package.template.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "libuchardet",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13)],
+    platforms: [.macOS(.v11), .iOS(.v14), .tvOS(.v14)],
     products: [
         .library(name: "Libuchardet", targets: ["_Libuchardet"]),
     ],
